114fe150da9b6ac30cc6386d68612baf5615fd78
Vegard Haugstvedt <it.vegard@gmail.com>
2019-10-24 12:19:40 +0200

    Added references for ToggleSwitch component
    Referred to Inclusive Components for JS/HTML code, and A11y Styled Form Controls for visual design.

--- src/components/toggle-switch/toggle-switch.js
+++ src/components/toggle-switch/toggle-switch.js
@@ -8,6 +8,14 @@ import "./toggle-switch.css"
 const getLabelId = id => `toggle-switch-${id}-label`
 const getHelpTextId = id => `toggle-switch-${id}-help`
 
+/**
+ * ToggleSwitch component
+ *
+ * Based on Heydon Pickering's Toggle Button from Inclusive Components [https://inclusive-components.design/toggle-button/]
+ * Visually inspired by Scott O'Hara's Toggle Switch component [https://scottaohara.github.io/a11y_styled_form_controls/src/toggle-button-switch/]
+ *
+ */
+
 const ToggleSwitch = ({ checked = false, helpText, id, onClick, label }) => {
   return (
     <div className="toggle-switch">
