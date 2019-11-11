7100385f03ebffb674d28dcbba3d9aa96a0e9403
Vegard Haugstvedt <it.vegard@gmail.com>
2019-11-11 07:38:07 +0100

    Add proper personal details form using our new components

--- src/components/personal-details-form/personal-details-form.js
+++ src/components/personal-details-form/personal-details-form.js
@@ -1,10 +1,20 @@
 import React from "react"
+import { TextInput } from "../input"
+import { Form, SubmitButton } from "../form"
 
 const PersonalDetailsForm = () => (
-  <form>
-    <label htmlFor="my-input">Label</label>
-    <input name="my-input" type="text" />
-  </form>
+  <Form action="/payment-details">
+    <TextInput autoComplete="given-name" name="first-name" label="First name" />
+    <TextInput autoComplete="family-name" name="last-name" label="Last name" />
+    <TextInput
+      autoComplete="street-address"
+      name="address-street"
+      label="Street"
+    />
+    <TextInput autoComplete="postal-code" name="address-zip" label="Zip-code" />
+    <TextInput autoComplete="address-level2" name="address-city" label="City" />
+    <SubmitButton type="submit">Go to payment</SubmitButton>
+  </Form>
 )
 
 export default PersonalDetailsForm
