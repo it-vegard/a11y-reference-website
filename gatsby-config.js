088ade739d80fba3929a721df96407ab78f62c55
Vegard Haugstvedt <it.vegard@gmail.com>
2019-11-11 07:33:37 +0100

    Update site name, description and author

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Accessible Clothes`,
+    description: `A fake web shop for testing and learning about accessibility.`,
+    author: `@it_vegard`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
