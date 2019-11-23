9236461c9b8a2d536e742b167d7580b75b9a5845
Till Halbach <halbach@nr.no>
2019-11-23 09:42:25 +0100

    added English and Norwegian language strings

--- src/data/texts/texts.en.js
+++ src/data/texts/texts.en.js
@@ -1,6 +1,40 @@
 const textsEN = {
+  // general
+  LANG: "English",
+  CURRENCY: "$",
+  // entire site
+  WEBSITE_NAME: "Clothes for all",
+  RULE_SET: "Ruleset",
+  SIZES: "Sizes",
+  ADD: "Add to cart",
+  CART: "Cart",
+  PAY: "Go to payment",
+  CONTINUE: "Continue",
+  // portal
   HOME: "Home",
   PRODUCT_FOR_GENDER_HEADING: gender => `${gender}'s Fashion`,
+  // personal details
+  PERS_DETAILS_TITLE: "Personal details",
+  PERS_DETAILS_HEADER: "Personal details",
+  PERS_DETAILS_FIRST_NAME: "First name",
+  PERS_DETAILS_LAST_NAME: "Last name",
+  PERS_DETAILS_STREET: "Street",
+  PERS_DETAILS_ZIP: "Zip code",
+  PERS_DETAILS_CITY: "City",
+  // payment details
+  PAY_DETAILS_TITLE: "Payment details",
+  PAY_DETAILS_HEADER: "Payment details",
+  PAY_DETAILS_TYPE: "Payment type",
+  PAY_DETAILS_CARD_NUMBER: "Card number",
+  PAY_DETAILS_EXPIRY_M: "Expiry month",
+  PAY_DETAILS_EXPIRY_Y: "Expiry year",
+  PAY_DETAILS_CSC: "CSC",
+  // receipt
+  RECEIPT_TITLE: "Receipt",
+  RECEIPT_HEADER: "Thank you for your purchase!",
+  RECEIPT_NOTE: "PS: You noticed this was not a real shop, right?",
+  RECEIPT_DISCLAIMER:
+    "No data has been sent from your computer. Data was preliminary stored in your browser while you were moving around the site but is now deleted. #PrivacyByDesign",
 }
 
 export default textsEN
--- src/data/texts/texts.no.js
+++ src/data/texts/texts.no.js
@@ -1,6 +1,39 @@
 const textsNO = {
+  // general
+  LANG: "norsk",
+  CURRENCY: "NOK",
+  // entire site
+  WEBSITE_NAME: "Klær for alle",
+  RULE_SET: "Regelsett",
+  SIZES: "Størrelser",
+  ADD: "Legg i handlekurven",
+  CART: "Handlekurv",
+  PAY: "Gjennomfør kjøpet",
+  CONTINUE: "Fortsett",
+  // portal
   HOME: "Hjem",
   PRODUCT_FOR_GENDER_HEADING: gender => `${gender}mote`,
+  // personal details
+  PERS_DETAILS_TITLE: "Persondata",
+  PERS_DETAILS_FIRST_NAME: "Fornavn",
+  PERS_DETAILS_LAST_NAME: "Etternavn",
+  PERS_DETAILS_STREET: "Gate",
+  PERS_DETAILS_ZIP: "Postkode",
+  PERS_DETAILS_CITY: "By",
+  // payment details
+  PAY_DETAILS_TITLE: "Betalingsdetaljer",
+  PAY_DETAILS_TYPE: "Betalingsmåte",
+  PAY_DETAILS_CARD_NUMBER: "Kortnummer",
+  PAY_DETAILS_EXPIRY_M: "Utløpsmåned",
+  PAY_DETAILS_EXPIRY_Y: "Utløpsår",
+  PAY_DETAILS_CSC: "CSC",
+  // receipt
+  RECEIPT_TITLE: "Kvittering",
+  RECEIPT_HEADER: "Mange takk for kjøpet ditt!",
+  RECEIPT_NOTE:
+    "PS: Du fikk med deg at dette ikke er en riktig nettbutikk, eller?",
+  RECEIPT_DISCLAIMER:
+    "Det ble ikke sendt noen data fra nettleseren din. Dataene ble lagret kun midlertidig i nettleseren og er nå slettet. #PrivacyByDesign",
 }
 
 export default textsNO
