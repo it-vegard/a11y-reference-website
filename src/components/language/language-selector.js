c2446a5f807176ed75b4d3ea78f4945616d60066
Vegard Haugstvedt <it.vegard@gmail.com>
2019-11-24 20:29:16 +0100

    Added flags for language picker

--- src/components/language/language-selector.js
+++ src/components/language/language-selector.js
@@ -1,12 +1,19 @@
 import React from "react"
+import NorwegianFlag from "svg-country-flags/svg/no.svg"
+import GreatBritishFlag from "svg-country-flags/svg/gb.svg"
+
 import Link from "../link"
 
 import "./language-selector.css"
 
 const LanguageSelector = () => (
   <div className="language-selector">
-    <Link title="NO" url="/no" />
-    <Link title="EN" url="/en" />
+    <Link className="language-selector__link" title="NO" url="/no">
+      <img className="language-selector__flag" src={NorwegianFlag} />
+    </Link>
+    <Link className="language-selector__link" title="EN" url="/en">
+      <img className="language-selector__flag" src={GreatBritishFlag} />
+    </Link>
   </div>
 )
 
--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,15 +1,19 @@
 import React from "react"
 import * as PropTypes from "prop-types"
+import classNames from "classnames"
 
 import "./link.css"
 
-const Link = ({ title, url }) => (
-  <a href={url} className="link">
+const Link = ({ children, className, title, url }) => (
+  <a href={url} className={classNames("link", { [className]: className })}>
     {title}
+    {children}
   </a>
 )
 
 Link.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
   title: PropTypes.string,
   url: PropTypes.string,
 }
