2e3e3b9b8e9c107cb55bc713f7e9c987bad27699
it-vegard <it.vegard@gmail.com>
2020-08-06 15:35:52 +0200

    Add "Link has role 'link' " rule

--- src/components/accessibility-configurator/accessibility-configurator.js
+++ src/components/accessibility-configurator/accessibility-configurator.js
@@ -39,7 +39,6 @@ const AccessibilityConfigurator = () => {
       allInternalRule {
         nodes {
           key
-          axeId
           wcagId
           description
           title
---
+++ src/data/rules/link-has-role-link.js
@@ -0,0 +1,8 @@
+const linkHasRoleLink = {
+  title: "Link has role 'link'",
+  description:
+    "Ensures links are correctly presented to assistive technology and are made available in lists of links on the page.",
+  wcagId: "1.3.1",
+}
+
+export default linkHasRoleLink
