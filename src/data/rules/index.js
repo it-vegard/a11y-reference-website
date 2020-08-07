d220993c8b4320ff026b8cb6d04a769b2b90977f
it-vegard <it.vegard@gmail.com>
2020-08-07 10:57:50 +0200

    Added "Group related links using the 'nav' element"-rule (1.3.1)

--- src/data/rules/index.js
+++ src/data/rules/index.js
@@ -4,12 +4,14 @@ import htmlHasLang from "./html-has-lang"
 import htmlLangValid from "./html-lang-valid"
 import imageAlt from "./image-alt"
 import linkHasRoleLink from "./link-has-role-link"
+import groupRelatedLinksUsingTheNavElement from "./group-related-links-using-the-nav-element"
 import headerHasRoleHeader from "./header-has-role-header"
 import label from "./label"
 
 const rules = [
   bypass,
   colorContrast,
+  groupRelatedLinksUsingTheNavElement,
   htmlHasLang,
   htmlLangValid,
   headerHasRoleHeader,
