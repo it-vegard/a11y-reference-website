48b1f10cf947ea3c8ceef277e4fefe171cba39d9
it-vegard <it.vegard@gmail.com>
2020-08-07 14:29:06 +0200

    Move logic for switching between inaccessible and accessible link components to the index file
    General rule-of-thumb going forward:
    Do not use context inside generic component, so it can be re-used in styleguide
    Make clearer separation of inaccessible and accessible code, so it is easier to find best practice and to read the code

--- src/components/link/inaccessible-link.js
+++ src/components/link/inaccessible-link.js
@@ -1,8 +1,14 @@
 import React from "react"
 import * as PropTypes from "prop-types"
+import classNames from "classnames"
 
-const InaccessibleLink = ({ children, url, ...rest }) => (
-  <span {...rest} onClick={() => (location.href = url)}>
+const InaccessibleLink = ({ children, className, title, url, ...rest }) => (
+  <span
+    {...rest}
+    className={classNames("link", { [className]: className })}
+    onClick={() => (location.href = url)}
+  >
+    {title}
     {children}
   </span>
 )
@@ -12,6 +18,8 @@ InaccessibleLink.propTypes = {
     PropTypes.node,
     PropTypes.arrayOf(PropTypes.node),
   ]),
+  className: PropTypes.string,
+  title: PropTypes.string,
   url: PropTypes.string,
 }
 
--- src/components/link/index.js
+++ src/components/link/index.js
@@ -1,3 +1,17 @@
-import Link from "./link"
+import React, { useContext } from "react"
+
+import CONSTANTS from "../../data/rules/constants"
+import AccessibilityRulesContext from "../accessibility-rules/accessibility-rules-context"
+import AccessibleLink from "./link"
+import InaccessibleLink from "./inaccessible-link"
+
+const Link = props => {
+  const { rules } = useContext(AccessibilityRulesContext)
+  if (!rules[CONSTANTS.LINK_HAS_ROLE_LINK]) {
+    return <InaccessibleLink {...props} />
+  } else {
+    return <AccessibleLink {...props} />
+  }
+}
 
 export default Link
