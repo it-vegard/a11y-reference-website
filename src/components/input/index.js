340a3f6a72d27e4e6bbf47f8b72d640b0e3ff8db
it-vegard <it.vegard@gmail.com>
2020-08-10 11:38:23 +0200

    Move use of context out of input component, also extracting accessibility rules to make the core component re-usable

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,3 +1,22 @@
-import TextInput from "./text-input"
+import React, { useContext } from "react"
+import * as PropTypes from "prop-types"
+
+import AccessibilityRulesContext from "../accessibility-rules/accessibility-rules-context"
+import AccessibleTextInput from "./text-input"
+import CONSTANTS from "../../data/rules/constants"
+
+const TextInput = ({ name, ...rest }) => {
+  const { rules } = useContext(AccessibilityRulesContext)
+  return (
+    <AccessibleTextInput
+      name={rules[CONSTANTS.LABEL] ? name : undefined}
+      {...rest}
+    />
+  )
+}
+
+TextInput.propTypes = {
+  name: PropTypes.string,
+}
 
 export { TextInput }
