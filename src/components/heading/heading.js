f77d0906ae74e6ba2a1f0366033a8852063e4fc9
it-vegard <it.vegard@gmail.com>
2020-08-12 11:05:26 +0200

    Move use of context out of heading component, also extracting accessibility rules to make the core component re-usable

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,41 +1,12 @@
-import React, { useContext } from "react"
-import classNames from "classnames"
+import React from "react"
 import * as PropTypes from "prop-types"
-
-import CONSTANTS from "../../data/rules/constants"
-import AccessibilityRulesContext from "../accessibility-rules/accessibility-rules-context"
 
 import "./heading.css"
 
-const headingStyles = [
-  "heading-largest",
-  "heading-larger",
-  "heading-large",
-  "heading-medium",
-  "heading-small",
-  "heading-smaller",
-]
-
-const Heading = ({
-  children,
-  className,
-  headingLevel,
-  headingStyle,
-  ...rest
-}) => {
-  const { rules } = useContext(AccessibilityRulesContext)
-  const HeadingWithLevel = rules[CONSTANTS.HEADER_HAS_ROLE_HEADER]
-    ? `h${headingLevel}`
-    : "span"
+const Heading = ({ children, className, headingLevel, ...rest }) => {
+  const HeadingWithLevel = `h${headingLevel}`
   return (
-    <HeadingWithLevel
-      className={classNames("heading", {
-        [headingStyles[headingLevel - 1]]: !headingStyle,
-        [headingStyle]: headingStyle,
-        [className]: className,
-      })}
-      {...rest}
-    >
+    <HeadingWithLevel className={className} {...rest}>
       {children}
     </HeadingWithLevel>
   )
@@ -45,7 +16,6 @@ Heading.propTypes = {
   children: PropTypes.node.isRequired,
   className: PropTypes.string,
   headingLevel: PropTypes.number.isRequired,
-  headingStyle: PropTypes.oneOf(headingStyles),
 }
 
 export default Heading
