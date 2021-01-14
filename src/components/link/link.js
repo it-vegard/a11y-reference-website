3c92ead57b77e16356b7fa707bfc789b5c0d87cc
it-vegard <it.vegard@gmail.com>
2021-01-14 23:40:03 +0100

    Make the link component render Gatsby Link for internal URLs and native links for external pages

--- src/components/link/link.js
+++ src/components/link/link.js
@@ -1,15 +1,31 @@
 import React from "react"
+import { Link as GatsbyLink } from "gatsby"
 import * as PropTypes from "prop-types"
 import classNames from "classnames"
 
 import "./link.css"
 
-const Link = ({ children, className, title, url }) => (
-  <a href={url} className={classNames("link", { [className]: className })}>
-    {title}
-    {children}
-  </a>
-)
+const isExternalURL = url => /^https?:\/\/|^\/\//i.test(url)
+
+const Link = ({ children, className, title, url }) => {
+  if (isExternalURL(url)) {
+    return (
+      <a href={url} className={classNames("link", { [className]: className })}>
+        {title}
+        {children}
+      </a>
+    )
+  }
+  return (
+    <GatsbyLink
+      to={url}
+      className={classNames("link", { [className]: className })}
+    >
+      {title}
+      {children}
+    </GatsbyLink>
+  )
+}
 
 Link.propTypes = {
   children: PropTypes.node,
