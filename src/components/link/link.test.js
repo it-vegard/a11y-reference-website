c9b029b24edfd964bdab76dc4d2f3b8c8a6f5fcf
Vegard Haugstvedt <it.vegard@gmail.com>
2022-02-17 02:19:34 +0100

    Add a basic test for the Link component, checking it has role="link"

---
+++ src/components/link/link.test.js
@@ -0,0 +1,33 @@
+import React from "react"
+import "@testing-library/jest-dom"
+import { render, screen, waitFor } from "@testing-library/react"
+import CONSTANTS from "../../data/rules/constants"
+import AccessibilityRulesContext from "../accessibility-rules/accessibility-rules-context"
+import Link from "./index"
+
+const defaultRules = {
+  [CONSTANTS.LINK_HAS_ROLE_LINK]: true,
+  [CONSTANTS.COLOR_CONTRAST]: true,
+}
+
+const renderLink = (text, rules) => (
+  <AccessibilityRulesContext.Provider
+    value={{
+      rules: {
+        ...defaultRules,
+        ...rules,
+      },
+    }}
+  >
+    <Link title={text} url="https://www.clothes4all.net" />
+  </AccessibilityRulesContext.Provider>
+)
+
+test("Link has role='link'", async () => {
+  render(renderLink("Go to Clothes4All.net"))
+
+  let link = null
+  await waitFor(() => (link = screen.getByRole("link")))
+
+  expect(link).toHaveTextContent("Go to Clothes4All.net")
+})
