7c1418ee94dd184a1a0aa9621e20e1b73bbc4097
Vegard Haugstvedt <it.vegard@gmail.com>
2022-02-25 21:35:44 +0100

    Fix cypress tests

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,5 @@
+import "cypress-axe"
+
 // ***********************************************************
 // This example support/index.js is processed and
 // loaded automatically before your test files.
